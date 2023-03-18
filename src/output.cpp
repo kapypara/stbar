58c7252e16322516aecdbc34f1ecbea1b9b1c35f
dio <shekel@cock.li>
2023-03-18 17:35:18 +0300

    [output.cpp]: Updated some stuff, idk this was done a long time ago

--- src/output.cpp
+++ src/output.cpp
@@ -1,26 +1,256 @@
 #include "output.h"
 #include "format.h"
 
+#include "widget/widgets_default.h"
+
 #include <chrono>
 #include <iostream>
+#include <locale>
+#include <codecvt>
+#include <future>
+
+template<typename segment_generic>
+void output_line::drawSegment(segment_generic const& seg, std::string const& str){
+    std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> converter;
+    std::u16string u16str = converter.from_bytes(str);
+
+    drawSegment(seg, u16str);
+}
+
+template<typename segment_generic>
+void output_line::drawSegment(segment_generic const& seg, std::u16string const& str){
+
+    if(str.empty())
+        return;
+
+    std::vector<float> characters;
+    std::vector<GLfloat> slots;
+
+    characters.reserve(seg.getMaxCharacters()*2);
+    slots.reserve(seg.getMaxCharacters()*2);
+
+    u16 limit = seg.getMaxCharacters() - seg.getOffset();
+
+    if(limit>str.size())
+        limit = str.size();
+
+    u16 k = 0;
+    for (u16 i=0; i<limit; ++i) {
+
+        auto const& c = str[i];
+
+        if(c == u'\n'){
+            continue;
+        }
+
+        const auto *x = font_cache.getGlyphCord(c);
+
+        if(x != nullptr) {
+            characters.push_back(x->x);
+            characters.push_back(x->y);
+
+#ifdef IGNORE_UNKNOWN_GLYPHS
+            slots.push_back(seg.getSlot(seg.getOffset()+i-k));
+#else
+            slots.push_back(seg.getSlot(seg.getOffset()+i));
+#endif
+            slots.push_back(seg.getYpos());
+        } else {
+            k++;
+        }
+
+    }
+
+    rect.setBackgroundColor(seg.getColor());
+    rect.setTextureCoordinates(characters);
+    rect.setRectPoints(slots);
+
+    rect.draw(slots.size()/2);
+
+};
+
+template<typename segment_generic>
+void output_line::clearSegment(segment_generic const& seg){
+
+    rect.setRectSize(
+            {
+                seg.getMaxCharacters()*2*static_cast<GLfloat>(font_cache.getCharWidth())/static_cast<GLfloat>(screen.get_width()),
+                2
+            });
+
+    rect.setBackgroundColor(seg.getColor());
+
+    const auto *cord = font_cache.getGlyphCord(' ');
+    rect.setTextureCoordinates({cord->x, cord->y});
+
+    rect.setRectPoints({seg.getSlot(0), 1});
+
+
+    rect.draw(1);
+
+    rect.setRectSize(
+            {
+                2*static_cast<GLfloat>(font_cache.getCharWidth())/static_cast<float>(screen.get_width()),
+                2*static_cast<GLfloat>(font_cache.getCharHeight())/static_cast<GLfloat>(screen.get_height()),
+            });
+};
+
+// sizes
 
 void output_line::segmentsInit(){
 
     const u8 update_count = update_interval + update_leftover;
 
-    data.io_read(tick_rate, update_count, segment::type::rate_of_change);
-    data.io_write(tick_rate, update_count, segment::type::rate_of_change);
+    const int screen_width = screen.get_width();
+    const int char_count = screen_width / font_cache.getCharWidth();
 
-    data.net_rx(tick_rate, update_count, segment::type::rate_of_change);
-    data.net_tx(tick_rate, update_count, segment::type::rate_of_change);
+    //
+    GLfloat y_pos = .9f - static_cast<float>(font_cache.getCharWidth()) / static_cast<float>(screen_width);
+    //
 
-    data.mem_dirty(tick_rate, update_count);
-    data.mem_free(tick_rate, update_count);
-    data.cpu_usage(tick_rate, update_count);
+    std::vector<GLfloat> slots;
 
-    data.load(tick_rate, update_count);
-    data.temp(tick_rate, update_count);
-    data.space_free(tick_rate, update_count);
+    for(int i=0; i<char_count+1; i++){
+
+        // cord in opengl
+        // -1 -------------- 0 -------------- 1
+
+        // cord here
+        //  0 -------------- 1 -------------- 2
+
+        GLfloat cord = static_cast<float>(font_cache.getCharWidth()*i) / static_cast<float>(screen_width/2) - 1.f;
+
+        slots.push_back(cord);
+    }
+
+    time = {
+        { slots.end()-time_segment.start_offset, slots.end()-time_segment.end_offset},
+        y_pos, time_segment.draw_offset,
+        time_segment.color
+    };
+    clearSegment(time);
+
+
+    free_memory = {
+        {slots.end()-free_memory_segment.start_offset, slots.end()-free_memory_segment.end_offset},
+        y_pos, free_memory_segment.draw_offset,
+        free_memory_segment.color
+    };
+    clearSegment(free_memory);
+    drawSegment(free_memory, u"\n\n,\n\n\n");
+
+
+    cpu_usage = {
+        {slots.end()-cpu_usage_segment.start_offset, slots.end()-cpu_usage_segment.end_offset},
+        y_pos, cpu_usage_segment.draw_offset,
+        cpu_usage_segment.color
+    };
+    clearSegment(cpu_usage);
+    drawSegment(cpu_usage, u"\n\n\n");
+
+
+    temperature = {
+        {slots.end()-temperature_segment.start_offset, slots.end()-temperature_segment.end_offset},
+        y_pos, temperature_segment.draw_offset,
+        temperature_segment.color
+    };
+    clearSegment(temperature);
+    drawSegment(temperature, u"\n\n");
+
+
+    system_load = {
+        {slots.end()-system_load_segment.start_offset, slots.end()-system_load_segment.end_offset},
+        y_pos, system_load_segment.draw_offset,
+        system_load_segment.color
+    };
+    clearSegment(system_load);
+    drawSegment(system_load, u"\n\n.\n\n");
+
+
+    free_space = {
+        {slots.end()-free_space_segment.start_offset, slots.end()-free_space_segment.end_offset},
+        y_pos, free_space_segment.draw_offset,
+        free_space_segment.color
+    };
+    clearSegment(free_space);
+    drawSegment(free_space, u"\n\n\n\n.\n\n\nG");
+
+
+    dirty_memory = {
+        {slots.end()-dirty_memory_segment.start_offset, slots.end()-dirty_memory_segment.end_offset},
+        y_pos, dirty_memory_segment.draw_offset,
+        dirty_memory_segment.color
+    };
+    clearSegment(dirty_memory);
+    drawSegment(dirty_memory, u"\n\n\n.\n\n");
+
+
+    io_write = {
+        {slots.end()-io_write_segment.start_offset, slots.end()-io_write_segment.end_offset},
+        y_pos, io_write_segment.draw_offset,
+        io_write_segment.color
+    };
+    clearSegment(io_write);
+    drawSegment(io_write, u"\n\n\n.\n\n");
+
+
+    io_read = {
+        {slots.end()-io_read_segment.start_offset, slots.end()-io_read_segment.end_offset},
+        y_pos, io_read_segment.draw_offset,
+        io_read_segment.color
+    };
+    clearSegment(io_read);
+    drawSegment(io_read, u"\n\n\n.\n\n");
+
+    net_tx = {
+        {slots.end()-net_tx_segment.start_offset, slots.end()-net_tx_segment.end_offset},
+        y_pos, net_tx_segment.draw_offset,
+        net_tx_segment.color
+    };
+    clearSegment(net_tx);
+    drawSegment(net_tx, u"\n\n\n.\n\n");
+
+    net_rx = {
+        {slots.end()-net_rx_segment.start_offset, slots.end()-net_rx_segment.end_offset},
+        y_pos, net_rx_segment.draw_offset,
+        net_rx_segment.color
+    };
+    clearSegment(net_rx);
+    drawSegment(net_rx, u"\n\n\n.\n\n");
+
+
+    sound_button = {
+        {slots.end()-sound_button_segment.start_offset, slots.end()-sound_button_segment.end_offset},
+        y_pos, sound_button_segment.draw_offset,
+        sound_button_segment.color
+    };
+    clearSegment(sound_button);
+    drawSegment(sound_button, u"");
+
+
+    window_title = {
+        {slots.begin(), slots.end()-window_title_segment.end_offset},
+        y_pos, window_title_segment.draw_offset,
+        window_title_segment.color
+    };
+    clearSegment(window_title);
+
+
+    // initing data
+    free_memory(tick_rate, update_count);
+    cpu_usage(tick_rate, update_count);
+
+    temperature(tick_rate, update_count);
+    system_load(tick_rate, update_count);
+
+    free_space(tick_rate, update_count);
+    dirty_memory(tick_rate, update_count);
+
+    io_write(tick_rate, update_count, segment::type::rate_of_change);
+    io_read(tick_rate, update_count, segment::type::rate_of_change);
+
+    net_rx(tick_rate, update_count, segment::type::rate_of_change);
+    net_tx(tick_rate, update_count, segment::type::rate_of_change);
 
 }
 
@@ -48,15 +278,18 @@ void output_line::output() {
     while(1) cycle();
 }
 
+/*
 inline void output_line::setSegment(std::string& segment, std::string str){
 
     segment = std::move(str);
     new_update = true;
 }
+*/
 
 inline void output_line::cycle() {
 
-    setSegment(bar.time, getDate());
+    drawSegment(time, getDate());
+    // setSegment(bar.time, getDate());
 
     for(u8 i=0; i < tick_rate; ++i){
         getData(i);
@@ -70,71 +303,222 @@ inline void output_line::cycle() {
 
 inline void output_line::getData(u8 i){
 
-    data.mem_free.new_tick(i, getFreeMemory());
-    data.cpu_usage.new_tick(i, getCpuUseage());
+    screen.handleEvents();
+    // screen.update();
+    if (screen.is_ButtonClicked()){
+        handleClick(screen.getClick_x(), screen.getClick_button());
+        screen.clearClick();
+    }
 
-    data.load.new_tick(i, getSystemLoad());
-    data.temp.new_tick(i, getSystemTemperature());
+    // free_space.new_tick(i, getFreeMemory());
+    auto a1 = std::async(&segment::new_tick, &free_memory, i, getFreeMemory());
+    auto a2 = std::async(&segment::new_tick, &cpu_usage, i, getCpuUseage());
 
-    data.space_free.new_tick(i, getFreeSpace());
-    data.mem_dirty.new_tick(i, getDirtyMemory());
+    auto a3 = std::async(&segment::new_tick, &temperature, i, getSystemTemperature());
+    auto a4 = std::async(&segment::new_tick, &system_load, i, getSystemLoad());
 
-    const io_t io_tmp = getIoStat();
+    auto a5 = std::async(&segment::new_tick, &free_space, i, getFreeSpace());
+    auto a6 = std::async(&segment::new_tick, &dirty_memory, i, getDirtyMemory());
 
-    data.io_read.new_tick(i, io_tmp.read);
-    data.io_write.new_tick(i, io_tmp.write);
+    auto a7 = std::async(
+            [&](){
+                const io_t io_tmp = getIoStat();
 
-    const net_t net_tmp = getNetStat();
+                io_write.new_tick(i, io_tmp.write);
+                io_read.new_tick(i, io_tmp.read);
+            });
 
-    data.net_rx.new_tick(i, net_tmp.rx);
-    data.net_tx.new_tick(i, net_tmp.tx);
+    auto a8 = std::async(
+            [&](){
+                const net_t net_tmp = getNetStat();
+
+                net_rx.new_tick(i, net_tmp.rx);
+                net_tx.new_tick(i, net_tmp.tx);
+            });
+
+    a1.wait();
+    a2.wait();
+    a3.wait();
+    a4.wait();
+    a5.wait();
+    a6.wait();
+    a7.wait();
+    a8.wait();
 
 }
 
-void output_line::testValueAndSetSegment(
-    std::string& segment,
-    segment::state& data,
-    u8 index,
-    std::function<std::string(u64)> fn
-){
+void output_line::handleClick(const u16 x_pos, const u8 button){
 
-    const u64 old_data = data.last;
-    const u64 new_data = data.getTransitionStep(index);
+    // clicked slot is from right
+    u16 clicked_slot = 1+ (screen.get_width() - x_pos) / static_cast<u16>(font_cache.getCharWidth());
+
+    switch(clicked_slot){
+        case 0 ... time_segment.start_offset:
+
+            std::cout << "cclip -i $(date +%s)" << std::endl;
+
+        break;
+
+        case free_memory_segment.end_offset+1 ... cpu_usage_segment.start_offset:
+
+            std::cout << "konsole -e htop &" << std::endl;
+
+        break;
+
+        case temperature_segment.end_offset+1 ... system_load_segment.start_offset:
+
+            std::cout << "screenfix &" << std::endl;
+
+        break;
+
+        case free_space_segment.end_offset+1 ... io_write_segment.start_offset:
+
+            std::cout << "dolphin &" << std::endl;
+
+        break;
+
+        case net_tx_segment.end_offset+1 ... net_rx_segment.start_offset:
+
+            std::cout << "usb-restart &" << std::endl;
+
+        break;
+
+        case sound_button_segment.end_offset+1 ... sound_button_segment.start_offset:
+
+            sound_button_state = !sound_button_state;
+            if(sound_button_state){
+                drawSegment(sound_button, u"\n");
+                std::cout << "line_out -s 1" << std::endl;
+
+            } else {
+                drawSegment(sound_button, u"\n");
+                std::cout << "line_out -s 0" << std::endl;
+
+            }
+
+        break;
+
+        default: // window title
+
+            std::cout << "cclip -i \"$(xdotool getwindowname `xdotool getactivewindow` | tr -d '\\n')\" &" << std::endl;
+
+        break;
+
+    }
+
+}
+
+void output_line::updateSegment(
+    segment& seg,
+    u8 index,
+    std::function<std::string(u64)> const& fn
+){
+    const u64 old_data = seg.getLast();
+    const u64 new_data = seg.getTransitionStep(index);
 
     if(new_data == old_data) { [[likely]]
         // std::cout << "skipping\n";
         return;
     }
 
-    setSegment(segment, fn(new_data));
+    drawSegment(seg, fn(new_data));
 }
+
+void output_line::updateSegment(
+    segment& seg,
+    u8 index,
+    std::function<std::u16string(u64)> const& fn
+){
+
+    const u64 old_data = seg.getLast();
+    const u64 new_data = seg.getTransitionStep(index);
+
+    if(new_data == old_data) { [[likely]]
+        // std::cout << "skipping\n";
+        return;
+    }
+
+    drawSegment(seg, fn(new_data));
+}
+
 
 void output_line::render(u8 index) {
 
-    testValueAndSetSegment(bar.mem_free, data.mem_free, index, format::memory);
-    testValueAndSetSegment(bar.cpu_usage, data.cpu_usage, index, format::cpu_useage);
 
-    testValueAndSetSegment(bar.net_rx, data.net_rx, index, format::generic);
-    testValueAndSetSegment(bar.net_tx, data.net_tx, index, format::generic);
+    updateSegment(free_memory, index, format::memory);
+    updateSegment(cpu_usage, index, format::cpu_useage);
 
-    testValueAndSetSegment(bar.io_write, data.io_write, index, format::generic);
-    testValueAndSetSegment(bar.io_read, data.io_read, index, format::generic);
+    updateSegment(temperature, index, format::none);
+    updateSegment(system_load, index, format::system_load);
 
-    testValueAndSetSegment(bar.mem_dirty, data.mem_dirty, index, format::generic);
-    testValueAndSetSegment(bar.space_free, data.space_free, index, format::free_space);
+    updateSegment(free_space, index, format::free_space);
+    updateSegment(dirty_memory, index, format::generic);
 
-    testValueAndSetSegment(bar.load, data.load, index, format::system_load);
-    testValueAndSetSegment(bar.temp, data.temp, index, format::none);
+    updateSegment(io_write, index, format::generic);
+    updateSegment(io_read, index, format::generic);
 
-    if(!window_name.not_changed.test_and_set(std::memory_order_consume)) {
-        setSegment(bar.window_title, window_name.getString());
+    updateSegment(net_tx, index, format::generic);
+    updateSegment(net_rx, index, format::generic);
+
+    if(!window_title_string.not_changed.test_and_set(std::memory_order_consume)) {
+
+        clearSegment(window_title);
+
+        std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> converter;
+        window_title_u16string = converter.from_bytes(window_title_string.getString());
+
+        // a hack to remove undrawable charaters from string
+        {
+            auto& new_name = window_title_u16string;
+
+            u16 i, k;
+
+            k = new_name.length();
+            i = 0;
+
+            while (i < k){
+
+                if (font_cache.getGlyphCord(new_name[i]) == nullptr){
+                    new_name.erase(new_name.begin()+i);
+                    k--;
+
+                } else {
+                    i++;
+                }
+            }
+        }
+
+
+        if(window_title_u16string.size() > window_title.getMaxCharacters()){
+            window_title_u16string.push_back(' ');
+            window_title_u16string.push_back(2);
+            window_title_u16string.push_back(' ');
+        }
+
+        window_fetch_time = program_counter;
+        window_name_rotation = 0;
+
+        drawSegment(window_title, window_title_u16string);
+
+    } else if (program_counter - window_fetch_time > static_cast<i64>(update_rate) &&
+        !((program_counter - window_fetch_time)%(update_interval))){
+
+        if(window_title_u16string.length() > window_title.getMaxCharacters()){
+
+            std::rotate(window_title_u16string.begin(), window_title_u16string.begin()+1, window_title_u16string.end());
+
+            drawSegment(window_title, window_title_u16string);
+
+            if(++window_name_rotation == window_title_u16string.size()*2){
+                window_fetch_time = program_counter + 60*update_rate;
+                window_name_rotation = 0;
+            }
+        }
     }
 
 
-    if (new_update){
-        bar.draw();
-        new_update = false;
-    }
+    glFlush();
 
+    program_counter++;
     sleep();
 }
--- src/output.h
+++ src/output.h
@@ -1,7 +1,8 @@
 #ifndef STBAR_V2_OUTPUT_H
 #define STBAR_V2_OUTPUT_H
 
-#include "segments.h"
+#include "segment.h"
+#include "renderer.h"
 #include "types.h"
 #include "widget/widgets.h"
 
@@ -12,11 +13,33 @@
 
 class output_line {
 
-    segment::output bar;
+    segment_draw time; // draw only
+    segment free_memory;
+    segment cpu_usage;
+    segment temperature;
+    segment system_load;
 
-    locked_string window_name;
+    segment free_space;
+    segment dirty_memory;
+    segment io_write;
+    segment io_read;
+
+    segment net_tx;
+    segment net_rx;
+
+    segment_draw sound_button;
+    segment_draw window_title;
+
+    bool sound_button_state = false;
+
+    locked_string window_title_string;
+    std::u16string window_title_u16string;
 
     bool new_update = false;
+
+    renderer& screen;
+    static_rect& rect;
+    font& font_cache;
 
     const u8 tick_rate, update_rate;
     const u8 update_interval, update_leftover;
@@ -24,44 +47,60 @@ class output_line {
 
     std::chrono::time_point<std::chrono::steady_clock> init_time;
 
-    std::thread sound_thread;
     std::thread window_thread;
 
-    // std::atomic_flag sound_state;
+    i64 program_counter;
 
     bool error = false; // for debugging
 
-    segment::data<segment::state> data;
-
     windowName active_window;
+    i64 window_fetch_time;
+    u64 window_name_rotation;
 
     void segmentsInit();
     void sleep();
 
     void cycle();
+    void handleClick(u16 x_pos, u8 button = 1);
     void getData(u8);
     void render(u8);
 
-    void setSegment(std::string&, std::string);
-    void testValueAndSetSegment(std::string&, segment::state&, u8, std::function<std::string(u64)>);
+    template<typename segment_generic>
+    void drawSegment(segment_generic const& seg, std::string const& str);
+
+    template<typename segment_generic>
+    void drawSegment(segment_generic const& seg, std::u16string const& str);
+
+    template<typename segment_generic>
+    void clearSegment(segment_generic const& seg);
+
+    void updateSegment(segment&, u8, std::function<std::string(u64)> const&);
+    void updateSegment(segment&, u8, std::function<std::u16string(u64)> const&);
+    // void setSegment(std::string&, std::string);
+    // void testValueAndSetSegment(std::string&, segment::state&, u8, std::function<std::string(u64)>);
 
 public:
 
     // TODO remove unused var's
-    output_line(u8 tick, u8 update)
-        : tick_rate(update)
-        , update_rate(update)
+    output_line(renderer &scrn, static_rect& prepeard_rect, font& font_cache,
+                u8 tick, u8 update)
+        : screen(scrn)
+        , rect(prepeard_rect)
+        , font_cache(font_cache)
+        , tick_rate(tick)
+        , update_rate( (update>tick)?update:tick )
         , update_interval(update_rate/tick_rate)
         , update_leftover(update_rate%tick_rate)
 
         , period(std::chrono::milliseconds((int)1e3 / update_rate))
-        , active_window(window_name)
+        , active_window(window_title_string)
     {
 
+        // std::cout << (long)update_interval << '\n';
         segmentsInit();
 
-        sound_thread = std::thread(setSoundBit, &bar.sound_button);
-        sound_thread.detach();
+        // sound_thread = std::thread(setSoundBit, &sound_button_flag);
+        // sound_thread.detach();
 
         window_thread = std::thread(&windowName::start, &active_window);
         window_thread.detach();
